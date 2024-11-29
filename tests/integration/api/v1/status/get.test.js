c51764df092df4bff6f934b04c52b0a700c53597
Lucas Pousa de Andrade <47056205+LucasPAndrade@users.noreply.github.com>
2024-11-29 17:16:03 +0000

    finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,13 @@
 test("GET to /api/v1/status should return 200 and message key", async () => {
   const response = await fetch(`${TEST_BASE_URL}/api/v1/status`);
   expect(response.status).toBe(200);
+
+  //Test "Updated At"
+  const responseBody = await response.json();
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
