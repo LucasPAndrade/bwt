8bb59a66e76b3da413df6ea73a4c39d6e5f20daf
Lucas Pousa de Andrade <47056205+LucasPAndrade@users.noreply.github.com>
2024-12-06 20:14:23 +0000

    adds `/api/v1/migrations` endpoint

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,17 @@
-module.exports = {
+const dotenv = require("dotenv");
+dotenv.config({
+  path: ".env.development",
+});
+
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: ".",
+});
+
+const jestConfig = createJestConfig({
   setupFiles: ["<rootDir>/jest.setup.js"],
-};
+  moduleDirectories: ["node_modules", "<rootDir>"],
+});
+
+module.exports = jestConfig;
