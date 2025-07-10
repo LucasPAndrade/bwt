60f6cbb213b11fa367bfe484c886641450a8b0e5
Lucas Pousa de Andrade <47056205+LucasPAndrade@users.noreply.github.com>
2025-07-10 00:51:20 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,26 +6,35 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public");
 });
 
-test("POST to /api/v1/migrations should return 200", async () => {
-  // eslint-disable-next-line no-undef
-  const response1 = await fetch(`${TEST_BASE_URL}/api/v1/migrations`, {
-    method: "POST",
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pendins migrations", () => {
+      test("For the first time", async () => {
+        // eslint-disable-next-line no-undef
+        const response1 = await fetch(`${TEST_BASE_URL}/api/v1/migrations`, {
+          method: "POST",
+        });
+
+        expect(response1.status).toBe(201);
+
+        const response1Body = await response1.json();
+
+        expect(Array.isArray(response1Body)).toBe(true);
+        expect(response1Body.length).toBeGreaterThan(0);
+      });
+
+      test("For the second time", async () => {
+        // eslint-disable-next-line no-undef
+        const response2 = await fetch(`${TEST_BASE_URL}/api/v1/migrations`, {
+          method: "POST",
+        });
+        expect(response2.status).toBe(200);
+
+        const response2Body = await response2.json();
+
+        expect(Array.isArray(response2Body)).toBe(true);
+        expect(response2Body.length).toBe(0);
+      });
+    });
   });
-  expect(response1.status).toBe(201);
-
-  const response1Body = await response1.json();
-
-  expect(Array.isArray(response1Body)).toBe(true);
-  expect(response1Body.length).toBeGreaterThan(0);
-
-  // eslint-disable-next-line no-undef
-  const response2 = await fetch(`${TEST_BASE_URL}/api/v1/migrations`, {
-    method: "POST",
-  });
-  expect(response2.status).toBe(200);
-
-  const response2Body = await response2.json();
-
-  expect(Array.isArray(response2Body)).toBe(true);
-  expect(response2Body.length).toBe(0);
 });
