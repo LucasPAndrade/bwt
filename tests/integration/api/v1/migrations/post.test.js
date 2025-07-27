1555b3f8f872d641a7821d4d9a8143e3e34619b3
Lucas Pousa de Andrade <47056205+LucasPAndrade@users.noreply.github.com>
2025-07-27 22:31:25 +0000

    refactor: rename variables and fix typos

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -7,38 +7,38 @@ beforeAll(async () => {
 
 describe("POST /api/v1/migrations", () => {
   describe("Anonymous user", () => {
-    describe("Running pendins migrations", () => {
+    describe("Running pending migrations", () => {
       test("For the first time", async () => {
         // eslint-disable-next-line no-undef
-        const response1 = await fetch(
+        const response = await fetch(
           "http://localhost:3000/api/v1/migrations",
           {
             method: "POST",
           },
         );
 
-        expect(response1.status).toBe(201);
+        expect(response.status).toBe(201);
 
-        const response1Body = await response1.json();
+        const responseBody = await response.json();
 
-        expect(Array.isArray(response1Body)).toBe(true);
-        expect(response1Body.length).toBeGreaterThan(0);
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBeGreaterThan(0);
       });
 
       test("For the second time", async () => {
         // eslint-disable-next-line no-undef
-        const response2 = await fetch(
+        const response = await fetch(
           "http://localhost:3000/api/v1/migrations",
           {
             method: "POST",
           },
         );
-        expect(response2.status).toBe(200);
+        expect(response.status).toBe(200);
 
-        const response2Body = await response2.json();
+        const responseBody = await response.json();
 
-        expect(Array.isArray(response2Body)).toBe(true);
-        expect(response2Body.length).toBe(0);
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBe(0);
       });
     });
   });
