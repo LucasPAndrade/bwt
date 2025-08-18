1989202ea7cba27a5e3cebd606c8ff5fbf0ca203
Lucas Pousa de Andrade <47056205+LucasPAndrade@users.noreply.github.com>
2025-08-18 04:31:50 +0000

    feat: create `user` model and `/api/v1/users` endpoint

---
+++ models/user.js
@@ -0,0 +1,128 @@
+import database from "infra/database.js";
+import { ValidationError } from "infra/errors.js";
+
+const REQUIRED_FIELDS = ["username", "email", "password"];
+
+function normalizeUserData(userInputValues) {
+  const normalizedEmail = normalizeEmail(userInputValues.email);
+  const normalizedUsername = normalizeUsername(userInputValues.username);
+
+  return {
+    ...userInputValues,
+    username_normalized: normalizedUsername,
+    email_normalized: normalizedEmail,
+  };
+}
+
+async function create(userInputValues) {
+  // --- Flow Management ---
+  // Orchestrates the overall process for creating a new user
+  const normalizedUser = normalizeUserData(userInputValues);
+  await validate(normalizedUser);
+
+  const newUser = await runInsertQuery(normalizedUser);
+  return newUser;
+
+  // --- Implementation Details ---
+  // Handles the underlying implementation details
+  // async function validateUniqueEmail(email) {}
+
+  async function runInsertQuery(user) {
+    const results = await database.query({
+      text: `
+      INSERT INTO 
+        users (username, username_normalized, email, email_normalized, password)
+      VALUES
+          ($1, $2, $3, $4, $5)
+      RETURNING
+        *
+        `,
+      values: [
+        user.username,
+        user.username_normalized,
+        user.email,
+        user.email_normalized,
+        user.password,
+      ],
+    });
+
+    return results.rows[0];
+  }
+}
+
+async function validate(normalizedUser) {
+  // --- Validação técnica ---
+  // Verificação de campos vazios ou nulos
+
+  const missingFields = REQUIRED_FIELDS.filter(
+    (field) =>
+      normalizedUser[field] === undefined || normalizedUser[field] === "",
+  );
+
+  if (missingFields.length > 0) {
+    const details = missingFields.reduce((acc, field) => {
+      acc[field] = `O campo ${field} é obrigatório.`;
+      return acc;
+    }, {});
+
+    throw new ValidationError({
+      message: "Validação de dados de entrada falhou devido a dados ausentes.",
+      details: details,
+    });
+  }
+
+  // --- Validação de regras de negócio ---
+  // E-mail duplicado
+  const existingEmail = await database.query({
+    text: "SELECT email_normalized FROM users WHERE email_normalized = $1",
+    values: [normalizedUser.email_normalized],
+  });
+
+  if (existingEmail.rowCount > 0) {
+    throw new ValidationError({
+      message: "O e-mail informado já está sendo utilizado.",
+      action: "Utilize outro e-mail para realizar o cadastro.",
+    });
+  }
+
+  // Username duplicado
+  const existingUser = await database.query({
+    text: "SELECT username_normalized FROM users WHERE username_normalized = $1",
+    values: [normalizedUser.username_normalized],
+  });
+
+  if (existingUser.rowCount > 0) {
+    throw new ValidationError({
+      message: "O username informado já está sendo utilizado.",
+      action: "Utilize outro username para realizar o cadastro.",
+    });
+  }
+}
+
+function normalizeEmail(email) {
+  if (!email) {
+    return email;
+  }
+  const [localPart, domain] = email.split("@");
+  const normalizedLocalPart = localPart
+    .replace(/\./g, "")
+    .split("+")[0]
+    .toLowerCase();
+  const normalizedDomain = domain.toLowerCase();
+
+  return `${normalizedLocalPart}@${normalizedDomain}`;
+}
+
+function normalizeUsername(username) {
+  if (!username) {
+    return username;
+  }
+  const normalizedUsername = username.toLowerCase();
+  return normalizedUsername;
+}
+
+const user = {
+  create,
+};
+
+export default user;
---
+++ tests/integration/api/v1/users/post.test.js
@@ -0,0 +1,170 @@
+import { version as uuidVersion } from "uuid";
+import orchestrator from "tests/orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigration();
+});
+
+//Define a base for a valid user, to be used in all invalid data test cases.
+const baseUser = {
+  username: "validUsername",
+  email: "validemail@mail.com",
+  password: "validpwd1",
+};
+
+describe("POST /api/v1/users", () => {
+  describe("Anonymous user", () => {
+    test("With unique and valid data", async () => {
+      // eslint-disable-next-line no-undef
+      const response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "lucasandrade",
+          email: "meulemail@mail.com",
+          password: "123QWE",
+        }),
+      });
+
+      expect(response.status).toBe(201);
+
+      const responseBody = await response.json();
+      expect(responseBody).toEqual({
+        id: responseBody.id,
+        username: "lucasandrade",
+        username_normalized: responseBody.username_normalized,
+        email: "meulemail@mail.com",
+        email_normalized: responseBody.email_normalized,
+        password: "123QWE",
+        created_at: responseBody.created_at,
+        updated_at: responseBody.updated_at,
+      });
+
+      expect(uuidVersion(responseBody.id)).toBe(4);
+      expect(Date.parse(responseBody.created_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+    });
+
+    test("With duplicated 'email'", async () => {
+      // eslint-disable-next-line no-undef
+      const response1 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "emailduplicado1",
+          email: "emailduplicado@mail.com",
+          password: "123QWE",
+        }),
+      });
+
+      expect(response1.status).toBe(201);
+
+      const response2 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "emailduplicado2",
+          email: "Emailduplicado@mail.com",
+          password: "123QWE",
+        }),
+      });
+
+      expect(response2.status).toBe(400);
+
+      const response2Body = await response2.json();
+      expect(response2Body).toEqual({
+        name: "ValidationError",
+        message: "O e-mail informado já está sendo utilizado.",
+        action: "Utilize outro e-mail para realizar o cadastro.",
+        status_code: 400,
+      });
+    });
+
+    test("With duplicated 'username'", async () => {
+      // eslint-disable-next-line no-undef
+      const response1 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "userduplicado",
+          email: "novouserduplicado@mail.com",
+          password: "123QWE",
+        }),
+      });
+
+      expect(response1.status).toBe(201);
+
+      const response2 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "Userduplicado",
+          email: "novouserduplicado2@mail.com",
+          password: "123QWE",
+        }),
+      });
+
+      expect(response2.status).toBe(400);
+
+      const response2Body = await response2.json();
+      expect(response2Body).toEqual({
+        name: "ValidationError",
+        message: "O username informado já está sendo utilizado.",
+        action: "Utilize outro username para realizar o cadastro.",
+        status_code: 400,
+      });
+    });
+
+    test("With missing data", async () => {
+      const invalidUserCases = [
+        {
+          description: "missing 'username'",
+          body: {
+            ...baseUser,
+            username: "",
+          },
+        },
+        {
+          description: "missing 'email'",
+          body: {
+            ...baseUser,
+            email: "",
+          },
+        },
+        {
+          description: "missing 'password'",
+          body: {
+            ...baseUser,
+            password: "",
+          },
+        },
+      ];
+
+      for (const testCase of invalidUserCases) {
+        // eslint-disable-next-line no-undef
+        const response = await fetch("http://localhost:3000/api/v1/users", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(testCase.body),
+        });
+
+        expect(response.status).toBe(400);
+        // console.log(`Test passed for ${testCase.description}`);
+      }
+    });
+  });
+});
